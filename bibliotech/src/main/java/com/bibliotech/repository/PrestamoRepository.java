f9b72b08e0422526e1f31e66f63e6ea8678959d1
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2026-04-24 20:46:09 -0300

    feat: add prestamo model, repository interface and in-memory implementation

---
+++ bibliotech/src/main/java/com/bibliotech/repository/PrestamoRepository.java
@@ -0,0 +1,13 @@
+package com.bibliotech.repository;
+
+import com.bibliotech.model.EstadoPrestamo;
+import com.bibliotech.model.Prestamo;
+
+import java.util.List;
+
+public interface PrestamoRepository extends Repository<Prestamo, Integer> {
+    List<Prestamo> buscarPorSocio(Integer socioId);
+    List<Prestamo> buscarPorRecurso(String isbn);
+    List<Prestamo> buscarPorEstado(EstadoPrestamo estado);
+
+}
